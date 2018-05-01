6b77794ca1eaa91faaee12cb9f0d8abac0c13fa8
Alex McKinney <mensch@uber.com>
2018-05-01 09:56:46 -0700

    Scaffold protoc plugin flow

--- main.go
+++ main.go
@@ -1,3 +1,41 @@
-package main
+package main // import github.com/amckinney/protoc-enums
 
-func main() {}
+import (
+	"bufio"
+	"io/ioutil"
+	"os"
+
+	plugin "github.com/golang/protobuf/protoc-gen-go/plugin"
+)
+
+func main() {
+	in := bufio.NewReader(os.Stdin)
+	bytes, err := ioutil.ReadAll(in)
+	if err != nil {
+		panic(err)
+	}
+
+	req := &plugin.CodeGeneratorRequest{}
+	if err := req.Unmarshal(bytes); err != nil {
+		panic(err)
+	}
+
+	res, err = generate(req)
+	if err != nil {
+		panic(err)
+	}
+
+	out, err := res.Marshal(make([]byte), true)
+	if err != nil {
+		panic(err)
+	}
+
+	_, err := os.Stdout.Write(out)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func generate(req *plugin.CodeGeneratorRequest) (*plugin.CodeGeneratorResponse, error) {
+	return &plugin.CodeGeneratorResponse{}, nil
+}
